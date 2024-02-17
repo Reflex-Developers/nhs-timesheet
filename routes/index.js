7392dec6d6949d9711dcc3069f05a1ae87b3c77f
wolwesking <83821890+wolwesking@users.noreply.github.com>
2024-02-17 21:36:10 +0500

    studentDisplay

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,75 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+
+// Global variables
+
+const dummyData = [
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+  { 
+    image: "/nhs-logo.png",
+    fullname: "Patrik Takacs",
+    lastTimePresent: "02/17/2024",
+    ranks: "VP, Member",
+    hours: "20.5h"
+  },
+];
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('pages/index', { title: 'home', activePage: 'home', firstname: "Patrik"});
+router.get("/", function (req, res, next) {
+  res.render("pages/index", {
+    title: "home",
+    activePage: "home",
+    firstname: "Patrik",
+    studentsData: dummyData,
+  });
 });
 
 module.exports = router;
