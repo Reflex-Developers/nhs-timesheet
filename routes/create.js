3dee2e378f0265f99bf36d68c80f3b6fe199544c
wolwesking <83821890+wolwesking@users.noreply.github.com>
2024-02-23 20:11:15 +0500

    createPageBasedOffArray

--- routes/create.js
+++ routes/create.js
@@ -3,12 +3,35 @@ const router = express.Router();
 
 // Global variables
 
+const studentsArray = [
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+"TestUser1",
+]
+
 /* GET home page. */
 router.get("/", function (req, res, next) {
   res.render("pages/create", {
     title: "create",
     activePage: "create",
-    firstname: "Patrik",
+    students: studentsArray,
   });
 });
 
